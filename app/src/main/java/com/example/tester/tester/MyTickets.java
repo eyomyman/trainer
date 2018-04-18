eac306aa2ef2998183acee1127f1fcb53f910878
Peteris Berzins <peteris@peteris.peteris>
2018-04-18 17:04:50 +0300

    Pre DB, front-end almost done

---
+++ app/src/main/java/com/example/tester/tester/MyTickets.java
@@ -0,0 +1,13 @@
+package com.example.tester.tester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTickets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_tickets);
+    }
+}
